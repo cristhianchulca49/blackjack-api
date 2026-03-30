8a0e27e8cc1d76c75652390fd1e2c6f1eeed1e22
cristhian <cristhianchulca49@gmail.com>
2026-03-30 23:11:51 +0200

    feat(persistence): add `PlayerRepositoryAdapter` and `PlayerR2dbcRepository` for player persistence
    - Implement `PlayerRepositoryAdapter` for reactive persistence operations with error handling.
    - Define `PlayerR2dbcRepository` as an interface for database interaction using R2DBC.

---
+++ blackjack-api/src/main/java/com/blackjack/player/infrastructure/adapter/out/persistence/PlayerRepositoryAdapter.java
@@ -0,0 +1,49 @@
+package com.blackjack.player.infrastructure.adapter.out.persistence;
+
+import com.blackjack.game.domain.model.exception.DomainException;
+import com.blackjack.player.domain.model.Player;
+import com.blackjack.player.domain.model.PlayerId;
+import com.blackjack.player.domain.port.out.PlayerRepositoryPort;
+import com.blackjack.player.infrastructure.adapter.out.persistence.repository.PlayerR2dbcRepository;
+import com.blackjack.player.infrastructure.mapper.PlayerMapper;
+import com.blackjack.shared.infrastructure.exception.DatabaseException;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public class PlayerRepositoryAdapter implements PlayerRepositoryPort {
+
+    private final PlayerR2dbcRepository playerR2dbcRepository;
+
+    public PlayerRepositoryAdapter(PlayerR2dbcRepository playerR2dbcRepository) {
+        this.playerR2dbcRepository = playerR2dbcRepository;
+    }
+
+    @Override
+    public Mono<Player> save(Player player) {
+        return Mono.just(player)
+                .map(PlayerMapper::toEntity)
+                .flatMap(playerR2dbcRepository::save)
+                .map(PlayerMapper::toDomain)
+                .onErrorMap(this::handleDatabaseError);
+    }
+
+    @Override
+    public Mono<Player> findById(PlayerId playerId) {
+        return playerR2dbcRepository.findById(playerId.getValue())
+                .map(PlayerMapper::toDomain)
+                .onErrorMap(this::handleDatabaseError);
+    }
+
+    @Override
+    public Mono<Void> deleteById(PlayerId playerId) {
+        return playerR2dbcRepository.deleteById(playerId.getValue())
+                .onErrorMap(this::handleDatabaseError);
+    }
+
+    private Throwable handleDatabaseError(Throwable ex) {
+        if (ex instanceof DomainException) return ex;
+        return new DatabaseException("Database failure while persisting player, error: " + ex.getMessage() );
+    }
+}
+
