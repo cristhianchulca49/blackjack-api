a9717c9d432ce7d8d37b4421085cdb993a13da89
cristhian <cristhianchulca49@gmail.com>
2026-03-30 23:11:51 +0200

    feat(persistence): add `PlayerRepositoryAdapter` and `PlayerR2dbcRepository` for player persistence
    - Implement `PlayerRepositoryAdapter` for reactive persistence operations with error handling.
    - Define `PlayerR2dbcRepository` as an interface for database interaction using R2DBC.

---
+++ blackjack-api/src/main/java/com/blackjack/player/infrastructure/adapter/out/persistence/repository/PlayerR2dbcRepository.java
@@ -0,0 +1,10 @@
+package com.blackjack.player.infrastructure.adapter.out.persistence.repository;
+
+import com.blackjack.player.domain.model.Player;
+import com.blackjack.player.infrastructure.adapter.out.persistence.entity.PlayerEntity;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerR2dbcRepository extends R2dbcRepository<PlayerEntity, String>{
+}
