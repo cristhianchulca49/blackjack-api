5b77f819464dc28c0f124ca2393c32a0833ca093
cristhian <cristhianchulca49@gmail.com>
2026-03-30 15:40:31 +0200

    feat(application): implement `GetGameByPlayerIdUseCaseImpl` to fetch games by player ID
    - Add `execute` method to retrieve games by player ID, validating player existence via `PlayerRepositoryPort`.
    - Throw `PlayerNotFoundException` if the player does not exist.

---
+++ blackjack-api/src/main/java/com/blackjack/game/application/usecase/GetGameByPlayerIdUseCaseImpl.java
@@ -0,0 +1,27 @@
+package com.blackjack.game.application.usecase;
+
+import com.blackjack.game.domain.model.Game;
+import com.blackjack.game.domain.port.in.GetGameByPlayerIdUseCase;
+import com.blackjack.game.domain.port.out.GameRepositoryPort;
+import com.blackjack.player.domain.model.PlayerId;
+import com.blackjack.player.domain.port.out.PlayerRepositoryPort;
+import com.blackjack.shared.application.exception.PlayerNotFoundException;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class GetGameByPlayerIdUseCaseImpl implements GetGameByPlayerIdUseCase {
+    private final GameRepositoryPort gameRepositoryPort;
+    private final PlayerRepositoryPort playerRepositoryPort;
+
+    public GetGameByPlayerIdUseCaseImpl(GameRepositoryPort gameRepositoryPort, PlayerRepositoryPort playerRepositoryPort) {
+        this.gameRepositoryPort = gameRepositoryPort;
+        this.playerRepositoryPort = playerRepositoryPort;
+    }
+
+    @Override
+    public Flux<Game> execute(PlayerId playerId) {
+        return playerRepositoryPort.findById(playerId.getValue())
+                .switchIfEmpty(Mono.error(new PlayerNotFoundException(playerId.getValue())))
+                .flatMapMany(player -> gameRepositoryPort.findByPlayerId(playerId.getValue()));
+    }
+}
