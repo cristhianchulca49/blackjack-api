dc981653797e11453cc3e311eb8a2d3e5a4b0190
cristhian <cristhianchulca49@gmail.com>
2026-03-30 21:39:36 +0200

    feat(exception): add `DatabaseException` for database-related errors
    - Introduce `DatabaseException` extending `RuntimeException` for centralized handling of database issues.
    - Remove unused `.gitkeep` file in the exceptions package.

---
+++ blackjack-api/src/main/java/com/blackjack/shared/infrastructure/exception/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.blackjack.shared.infrastructure.exception;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
