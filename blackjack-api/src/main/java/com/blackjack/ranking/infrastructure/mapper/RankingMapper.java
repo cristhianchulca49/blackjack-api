6ab84496182bd68dc1527147fc66f4cb789b527e
cristhian <cristhianchulca49@gmail.com>
2026-03-31 17:16:16 +0200

    feat(persistence): implement `RankingMapper` and `RankingRepositoryAdapter` for ranking persistence
    - Add `RankingMapper` to handle conversions between `Ranking` domain model and `RankingEntity`.
    - Implement `RankingRepositoryAdapter` to provide reactive operations for saving, retrieving player rankings, and querying top rankings.

---
+++ blackjack-api/src/main/java/com/blackjack/ranking/infrastructure/mapper/RankingMapper.java
@@ -0,0 +1,39 @@
+package com.blackjack.ranking.infrastructure.mapper;
+
+import com.blackjack.player.domain.model.Name;
+import com.blackjack.player.domain.model.PlayerId;
+import com.blackjack.ranking.domain.model.Ranking;
+import com.blackjack.ranking.domain.model.valueObject.Draws;
+import com.blackjack.ranking.domain.model.valueObject.Position;
+import com.blackjack.ranking.domain.model.valueObject.RankingId;
+import com.blackjack.ranking.domain.model.valueObject.Score;
+import com.blackjack.ranking.domain.model.valueObject.Wins;
+import com.blackjack.ranking.infrastructure.adapter.out.persistence.entity.RankingEntity;
+
+public class RankingMapper {
+
+    public static RankingEntity toEntity(Ranking ranking) {
+        return RankingEntity.builder()
+                .id(ranking.getId().getValue())
+                .playerId(ranking.getPlayerId().getValue())
+                .playerName(ranking.getPlayerName().toString())
+                .wins(ranking.getWins().getValue())
+                .draws(ranking.getDraws().getValue())
+                .score(ranking.getScore().getValue())
+                .position(ranking.getPosition().getValue())
+                .build();
+    }
+
+    public static Ranking toDomain(RankingEntity rankingEntity) {
+        return Ranking.from(
+                RankingId.reconstitute(rankingEntity.getId()),
+                PlayerId.reconstitute(rankingEntity.getPlayerId()),
+                Name.of(rankingEntity.getPlayerName()),
+                Wins.of(rankingEntity.getWins()),
+                Draws.of(rankingEntity.getDraws()),
+                Score.of(rankingEntity.getScore()),
+                Position.of(rankingEntity.getPosition())
+        );
+    }
+}
+
